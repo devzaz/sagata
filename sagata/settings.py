8d578bbc2e5e2052d8cc1f8fc1cb5bd21449809d
devzaz <zubaerahmedjisan@gmail.com>
2024-11-27 01:38:36 +0600

    Update settings.py

--- sagata/settings.py
+++ sagata/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-8pb7t*93bp!41q859ahrhg=t7*62ddr56^uqd(j-fupvis6nc)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["sagatak.com","buycloud.host", '*']
+ALLOWED_HOSTS = ["http://sagatak.com/"]
 
 
 # Application definition
